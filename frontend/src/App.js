6219b9c38a546c14267abdfeb84e03b050587f78
Danila V. Ovchinnikov <DVOvchinnikov@nota.tech>
2024-10-15 13:31:11 +0300

    no message

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello world!</div>,
+    },
+]);
+
+function App() {
+    return (
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
+    );
+}
+
+export default App;
