ea2bcf728e5fd16c50b8e5a0ab36fb0be3c0151d
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2024-11-01 11:53:28 +0300

    fix: обновлен сервер

--- backend/routes/server.js
+++ backend/routes/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { Server, User, Channel } = require('../models'); // Импортируйте модель Channel
+const { Server, User, Channel, ServerMember } = require('../models'); // Include ServerMember model
 const authenticateToken = require('../middleware/auth');
 
 const router = express.Router();
@@ -15,22 +15,31 @@ router.get('/', authenticateToken, async (req, res) => {
     }
 });
 
-// Создать новый сервер
+// Создать новый сервер и добавить создателя как владельца
 router.post('/', authenticateToken, async (req, res) => {
     const { name, description, icon } = req.body;
     try {
+        // Создаем новый сервер
         const newServer = await Server.create({ name, ownerId: req.user.userId, description, icon });
+        
+        // Добавляем создателя как участника с ролью владельца
+        await ServerMember.create({
+            userId: req.user.userId,
+            serverId: newServer.id,
+            role: 'owner',
+        });
+
         res.status(201).json(newServer);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 });
 
-// Получить сервер по ID и каналы
+// Получить сервер по ID и его каналы
 router.get('/:id', authenticateToken, async (req, res) => {
     try {
         const server = await Server.findByPk(req.params.id, {
-            include: [{ model: Channel, as: 'channels' }] // Включаем каналы
+            include: [{ model: Channel, as: 'channels' }], // Включаем каналы
         });
         if (!server) {
             return res.status(404).json({ message: 'Server not found' });
@@ -68,10 +77,15 @@ router.put('/:id', authenticateToken, checkServerOwnership, async (req, res) => 
     }
 });
 
-// Удалить сервер по ID
+// Удалить сервер по ID, включая все связи участников
 router.delete('/:id', authenticateToken, checkServerOwnership, async (req, res) => {
     try {
+        // Удалить все связи участников, связанных с сервером
+        await ServerMember.destroy({ where: { serverId: req.params.id } });
+
+        // Удалить сервер
         await req.server.destroy();
+
         res.status(204).send();
     } catch (error) {
         res.status(500).json({ error: error.message });
