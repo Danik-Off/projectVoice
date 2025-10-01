637867aae3c6baf337198bd4c9627ae3625f4c18
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2025-10-01 03:50:01 +0300

    refactor:  от чистого фронтенда решено было отказаться теперь он в рамках electron приложения

--- frontend/src/types/channelMember.ts
+++
@@ -1,6 +0,0 @@
-export interface ChannelMember {
-    id: number;            // Уникальный идентификатор участника канала
-    channelId: number;     // Идентификатор канала
-    userId: number;        // Идентификатор пользователя
-    role: string;          // Роль участника в канале (например, 'admin', 'member')
-}
